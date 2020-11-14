09a2f4f4641319387310a44b4f48f1e0b4b0ff74
kevinmilet <kevin.milet@gmail.com>
2020-11-14 15:54:50 +0100

    up fichiers

---
+++ Ex_json/assets/js/script.js
@@ -0,0 +1,27 @@
+// document.getElementById('aaa').innerHTML = data[0].name;
+
+// fetch("data.json")
+// document.getElementById('bbb').innerHTML = data[0].detail;
+
+// var img = document.createElement("img");
+//     img.src = data[0].image;
+// document.getElementById('ccc').appendChild(img);
+
+
+fetch('assets/json/productCatalog.json')
+    .then(response => response.json())
+    .then(json => document.getElementById('cardTitle').innerHTML = json[0].name);
+
+fetch('assets/json/productCatalog.json')
+    .then(response => response.json())   
+    .then(json => document.getElementById('cardContent').innerHTML = json[0].detail);
+
+fetch('assets/json/productCatalog.json')
+    .then(response => response.json())
+    .then(json => {
+        let img = document.createElement('img');
+        img.src = json[0].image;
+        document.getElementById('cardImg').appendChild(img);
+    });
+
+
---
+++ Ex_json/assets/json/productCatalog.js
@@ -0,0 +1,42 @@
+const data = [{
+	"name": "hero Product",
+	"detail": "Lorem ipsum dolor sit amet",
+	"price": "99",
+	"hero": "OMG This just came out today!",
+	"image": "https://picsum.photos/600/450"
+}, {
+	"name": "Product 1",
+	"detail": "Lorem ipsum dolor sit amet",
+	"price": "99",
+	"info": "This is the latest and greatest product from Derp corp.",
+	"image": "https://picsum.photos/600/450"
+}, {
+	"name": "Product 2",
+	"detail": "Lorem ipsum dolor sit amet",
+	"price": "99",
+	"offer": "BOGOF",
+	"image": "https://picsum.photos/600/450"
+}, {
+	"name": "Product 3",
+	"detail": "Lorem ipsum dolor sit amet",
+	"price": "99",
+	"image": "https://picsum.photos/600/450"
+}, {
+	"name": "Product 4",
+	"detail": "Lorem ipsum dolor sit amet",
+	"price": "99",
+	"offer": "No srsly GTFO",
+	"image": "https://picsum.photos/600/450"
+}, {
+	"name": "Product 5",
+	"detail": "Lorem ipsum dolor sit amet",
+	"price": "99",
+	"image": "https://picsum.photos/600/450"
+}, {
+	"name": "Product 6",
+	"detail": "Lorem ipsum dolor sit amet",
+	"price": "99",
+	"info": "This is the latest and greatest product from Derp corp.",
+	"offer": "info with offer",
+	"image": "https://picsum.photos/600/450"
+}]
