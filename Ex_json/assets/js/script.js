f754e376ec8e5b188d21c9423d98908e300af8a4
kevinmilet <kevin.milet@gmail.com>
2020-11-14 17:08:38 +0100

    donc je mets

--- Ex_json/assets/js/script.js
+++ Ex_json/assets/js/script.js
@@ -1,25 +1,20 @@
-// document.getElementById('aaa').innerHTML = data[0].name;
-
-// fetch("data.json")
-// document.getElementById('bbb').innerHTML = data[0].detail;
-
-// var img = document.createElement("img");
-//     img.src = data[0].image;
-// document.getElementById('ccc').appendChild(img);
-
-
-fetch('assets/json/productCatalog.json')
-    .then(response => response.json())
-    .then(json => document.getElementById('cardTitle').innerHTML = json[0].name);
-
-fetch('assets/json/productCatalog.json')
-    .then(response => response.json())   
-    .then(json => document.getElementById('cardContent').innerHTML = json[0].detail);
-
+// on récupère le fichier json
 fetch('assets/json/productCatalog.json')
     .then(response => response.json())
     .then(json => {
+        
+        // on récupère l'entrée name de l'id 0
+        // et on l'affiche
+        document.getElementById('cardTitle').innerHTML = json[0].name;
+        
+        // on récupère l'entrée detail de l'id 0
+        // et on l'affiche
+        document.getElementById('cardContent').innerHTML = json[0].detail;
+
+        // on crée un élément img qu'on stocke dans une variable
         let img = document.createElement('img');
+        // on aajoute la source de l'image grace à l'entrée image de l'index 0
         img.src = json[0].image;
+        // on affiche l'image
         document.getElementById('cardImg').appendChild(img);
     });
---
+++ Ex_json/assets/js/script_old.js
@@ -0,0 +1,15 @@
+// fetch('assets/json/productCatalog.json')
+//     .then(response => response.json())
+//     .then(json => document.getElementById('cardTitle').innerHTML = json[0].name);
+
+// fetch('assets/json/productCatalog.json')
+//     .then(response => response.json())   
+//     .then(json => document.getElementById('cardContent').innerHTML = json[0].detail);
+
+// fetch('assets/json/productCatalog.json')
+//     .then(response => response.json())
+//     .then(json => {
+//         let img = document.createElement('img');
+//         img.src = json[0].image;
+//         document.getElementById('cardImg').appendChild(img);
+//     });
