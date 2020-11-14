1838c5cdfebbb981793c285982e424da765c6c07
kevinmilet <kevin.milet@gmail.com>
2020-11-14 17:45:54 +0100

    modif

--- Ex_json/assets/js/script.js
+++ Ex_json/assets/js/script.js
@@ -1,4 +1,8 @@
+let myCard = '';
+
 // on récupère le fichier json
+
+
 fetch('assets/json/productCatalog.json')
     .then(response => response.json())
     .then(json => {
@@ -11,10 +15,11 @@ fetch('assets/json/productCatalog.json')
         // et on l'affiche
         document.getElementById('cardContent').innerHTML = json[0].detail;
 
-        // on crée un élément img qu'on stocke dans une variable
-        let img = document.createElement('img');
+        // on récupère l'entrée price de l'id 0
+        // et on l'affiche
+        document.getElementById('cardPrice').innerHTML = json[0].price + ' €';
+       
         // on aajoute la source de l'image grace à l'entrée image de l'index 0
-        img.src = json[0].image;
         // on affiche l'image
-        document.getElementById('cardImg').appendChild(img);
+        document.getElementById('cardImg').src = json[0].image;
     });
