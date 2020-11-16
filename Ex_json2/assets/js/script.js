f7c0f6b4e9872b588936ca7a7b2e1defc7ce273b
kevinmilet <kevin.milet@gmail.com>
2020-11-16 15:58:36 +0100

    up fichiers

--- Ex_json2/assets/js/script.js
+++ Ex_json2/assets/js/script.js
@@ -1,42 +1,10 @@
-// initialisation de la variable qui incrémentera les ids
-let newId = 0;
+function successCB(data) {
+    console.log("Success callback: " + data);
+};
 
-// on récupère le fichier json
-// et on boucle pour récupérer les données
-// pour les afficher sur les différentes card
-fetch('assets/json/productCatalog.json')
-    .then(response => response.json())
-    .then(json => {
+function errorCB(data) {
+    console.log("Error callback: " + data);
+};
 
-        // on boucle sur le tableau récupéré grace au fetch()
-        json.forEach((element) => {
 
-            // on récupère l'id la card
-            // et on la stocke dans une variable
-            let colCard = document.getElementById('colCard');
-
-            // on clone la card
-            let colCardClone = colCard.cloneNode(true);
-
-            // on incrémente les ids des éléments html de la card
-            // et on ajoute le contenu
-            colCardClone.id = 'colCardClone' + newId;
-            document.querySelector('.row').appendChild(colCardClone);
-
-            colCardClone.querySelector('#cardImg').id = 'cardImg' + newId;
-            colCardClone.querySelector('#cardImg' + newId).src = element.image + '?' + newId;
-
-            colCardClone.querySelector('#cardTitle').id = 'cardTitle' + newId;
-            colCardClone.querySelector('#cardTitle' + newId).innerHTML = element.name;
-
-            colCardClone.querySelector('#cardPrice').id = 'cardPrice' + newId;
-            colCardClone.querySelector('#cardPrice' + newId).innerHTML = element.price + ' €';
-
-            colCardClone.querySelector('#cardContent').id = 'cardContent' + newId;
-            colCardClone.querySelector('#cardContent' + newId).innerHTML = element.detail;
-
-            // on incrémente l'id
-            newId++
-        });
-
-    });
+console.log(theMovieDb.find.getById({ "id": "tt0993846", "external_source": "imdb_id" }, successCB, errorCB));
