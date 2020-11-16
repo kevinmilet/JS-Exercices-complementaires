bbd337380d73da8998c3fcdcf93ad2706b6f221e
kevinmilet <kevin.milet@gmail.com>
2020-11-16 20:27:30 +0100

    ...

--- Ex_json2/assets/js/script.js
+++ Ex_json2/assets/js/script.js
@@ -5,12 +5,14 @@ function successCB(data) {
     document.write(film.title + '<br>');
     document.write(film.genres[0].name + '<br>');
     document.write(film.overview + '<br>');
-    console.log('https://image.tmdb.org/t/p/w500/'+film.poster_path);
-    document.getElementById('img').src = '//image.tmdb.org/t/p/w500/'+film.poster_path;
+    console.log(`https://image.tmdb.org/t/p/w500/${film.poster_path}`);
+    document.getElementById("img").src.src = `https://image.tmdb.org/t/p/w500/${film.poster_path}`;
 };
 
 function errorCB(data) {
     console.log("Error callback: " + data);
 };
 
-theMovieDb.movies.getById({ "id": 76200 }, successCB, errorCB);
+theMovieDb.movies.getById({
+    "id": 76203
+}, successCB, errorCB);
