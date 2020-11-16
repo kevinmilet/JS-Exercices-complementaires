ae62bbe8dc754972698c0c906103590615512e8d
kevinmilet <kevin.milet@gmail.com>
2020-11-16 20:09:23 +0100

    ...

--- Ex_json2/assets/js/script.js
+++ Ex_json2/assets/js/script.js
@@ -1,18 +1,16 @@
 function successCB(data) {
-    console.log("Success callback: " + data);
+    // console.log("Success callback: " + data);
     let film = JSON.parse(data);
     console.log(film);
     document.write(film.title + '<br>');
     document.write(film.genres[0].name + '<br>');
+    document.write(film.overview + '<br>');
     console.log(film.backdrop_path);
-    document.querySelector('img').src = 'https://image.tmdb.org/t/p/w500' + film.backdrop_path;
+    // document.getElementById('img').src = 'https://image.tmdb.org/t/p/w500'+film.backdrop_path;
 };
 
 function errorCB(data) {
     console.log("Error callback: " + data);
 };
 
-
-// console.log(theMovieDb.movies.getById({ "id": 76203 }, successCB, errorCB));
-
-theMovieDb.movies.getById({ "id": 76203 }, successCB, errorCB);
+theMovieDb.movies.getById({ "id": 76200 }, successCB, errorCB);
