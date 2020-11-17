d39689671548242344320de00f5906cac74ce4e4
kevinmilet <kevin.milet@gmail.com>
2020-11-17 10:02:57 +0100

    ...

--- Ex_json2/assets/js/script.js
+++ Ex_json2/assets/js/script.js
@@ -1,18 +1,27 @@
-function successCB(data) {
-    // console.log("Success callback: " + data);
-    let film = JSON.parse(data);
-    console.log(film);
-    document.write(film.title + '<br>');
-    document.write(film.genres[0].name + '<br>');
-    document.write(film.overview + '<br>');
-    console.log(`https://image.tmdb.org/t/p/w500/${film.poster_path}`);
-    document.getElementById("img").src.src = `https://image.tmdb.org/t/p/w500/${film.poster_path}`;
-};
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
 
-function errorCB(data) {
-    console.log("Error callback: " + data);
-};
+    function successCB(data) {
+        // console.log("Success callback: " + data);
+        let film = JSON.parse(data);
+        console.log(film);
+        document.write(film.title + '<br>');
+        document.write(film.genres[0].name + '<br>');
+        document.write(film.overview + '<br>');
+        console.log(`https://image.tmdb.org/t/p/w500/${film.poster_path}`);
+        console.log(document.getElementById('img'));
+        document.getElementById("img").src = `https://image.tmdb.org/t/p/w500/${film.poster_path}`;
+    };
 
-theMovieDb.movies.getById({
-    "id": 76203
-}, successCB, errorCB);
+    function errorCB(data) {
+        console.log("Error callback: " + data);
+    };
+
+    theMovieDb.movies.getById({
+        "id": 76203
+    }, successCB, errorCB);
+})
+
+
+
+// console.log(document.getElementById('img'));
