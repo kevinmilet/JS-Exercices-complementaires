adc28c9a80025921bf2a7ebe9eac777b471d85b8
kevinmilet <kevin.milet@gmail.com>
2020-11-28 23:22:11 +0100

    ...

--- Ex_json2/assets/js/script.js
+++ Ex_json2/assets/js/script.js
@@ -1,9 +1,12 @@
 const api_key = 'e1244bd8733b1e1b4c4c313ea409e081';
-let movieNameStr = document.querySelector('#searchBar').value;
-const searchBtn = document.querySelector('#searchBtn');
+const secure_base_url ='https://image.tmdb.org/t/p/'
+let movieSearch = document.querySelector('#searchBar');
+let searchBtn = document.querySelector('#searchBtn');
 
-// console.log(movieNameStr.value)
-searchBtn.addEventListener('click', getMovie(movieNameStr));
+searchBtn.addEventListener('click', function(){
+    getMovie(movieSearch.value);
+});
+
 
 function movie(id, title, overview, poster_path, release_date) {
     this.id = id;
@@ -15,7 +18,7 @@ function movie(id, title, overview, poster_path, release_date) {
 }
 
 function getMovie(movieNameStr) {
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=fr-FR&query=${movieNameStr}&page=1&include_adult=false`)
+    return fetch(`https://api.themoviedb.org/3/search/multi?api_key=${api_key}&language=fr-FR&query=${movieNameStr}&page=1&include_adult=false`)
     .then(function(response) {
         return response.json();
     })
@@ -33,5 +36,9 @@ function getMovie(movieNameStr) {
         console.log(movieDetails.overview);
         console.log(movieDetails.release_date);
         console.log(movieDetails.poster_path);
+        document.querySelector('.title').innerHTML = movieDetails.title;
+        document.querySelector('.release_date').innerHTML = movieDetails.release_date;
+        document.querySelector('.overview').innerHTML = movieDetails.overview;
+        document.querySelector('.img').src = `${secure_base_url}w185/${movieDetails.poster_path}`;
     })
 }
